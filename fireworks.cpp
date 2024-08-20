6425588043d87d7bd17acf20bec664696482074a
DiggsPapu <diggs.papu@gmail.com>
2024-08-20 07:09:50 -0600

    feat: add Particle class for explosions
    Added class to make the explosion animation

--- fireworks.cpp
+++ fireworks.cpp
@@ -4,6 +4,42 @@
 #define WIDTH 1024 
 #define HEIGHT 768 
 
+class Particula {
+    public:
+        float x, y; 
+        float vx, vy;
+        int life;
+        int r, g, b;
+        int size;
+
+    Particula(float x, float y) {
+        this->x = x;
+        this->y = y;
+        this->life = rand() % 100 + 50; 
+
+        // Generar velocidad aleatoria
+        this->vx = rand() % 5 - 2;
+        this->vy = rand() % 5 - 2;
+
+        // Color aleatorio
+        this->r = rand() % 256;
+        this->g = rand() % 256;
+        this->b = rand() % 256;
+
+        this->size = rand() % 3 + 1;
+    }
+
+    void update() {
+        x += vx;
+        y += vy;
+        life--;
+    }
+
+    bool isAlive() {
+        return life > 0;
+    }
+};
+
 int main(int argc, char* argv[]) {
     // Inicialización de SDL
     if (SDL_Init(SDL_INIT_VIDEO) < 0) {
